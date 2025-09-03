9bb765140d15e1c33c265b1fc1c9d5fe08ba37f3
Yukito Hanamura <184593637+yukito0909@users.noreply.github.com>
2025-09-03 14:53:43 +0900

    多くのボタンの作成を行いました。

--- app/src/main/java/jp/ac/meijou/android/s241205144/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205144/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.s241205144;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205144.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205144.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        binding.buttonA.setOnClickListener(v -> {
+            var intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonB.setOnClickListener(v -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://abehiroshi.la.coocan.jp/"));
+            startActivity(intent);
+
         });
     }
 }
