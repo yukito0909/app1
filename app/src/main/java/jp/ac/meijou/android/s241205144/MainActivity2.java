860698012d830a63362ecf595c0d7240e51a1c23
Yukito Hanamura <184593637+yukito0909@users.noreply.github.com>
2025-09-03 16:36:17 +0900

    ボタンの作成し、OKを押すとOKがキャンセルを押すとcanceledがでるように変更を行いました。

--- app/src/main/java/jp/ac/meijou/android/s241205144/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205144/MainActivity2.java
@@ -1,20 +1,45 @@
 package jp.ac.meijou.android.s241205144;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s241205144.databinding.ActivityMain2Binding;
 import jp.ac.meijou.android.s241205144.databinding.ActivityMainBinding;
 
 public class MainActivity2 extends AppCompatActivity {
     private ActivityMain2Binding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result ->{
+                switch (result.getResultCode()){
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textView6.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.textView6.setText("Result: Canceled");
+
+                    }
+                    default -> {
+                        binding.textView6.setText("Result: Inkown(" + result.getResultCode() + ")");
+                    }
+            }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +63,17 @@ public class MainActivity2 extends AppCompatActivity {
             intent.setData(Uri.parse("http://abehiroshi.la.coocan.jp/"));
             startActivity(intent);
 
+        });
+
+        binding.button18.setOnClickListener( v -> {
+            var text = binding.editTextText2.getText().toString();
+            var intent = new Intent(this, MainActivity4.class);
+            intent.putExtra("title", text);
+            startActivity(intent);
+        });
+        binding.button19.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
         });
     }
 }
